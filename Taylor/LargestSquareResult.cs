4e03bda834c621b5e5511aade734a3ac6bc4e0d1
Stuart Leeks <stuartle@microsoft.com>
2019-02-19 20:30:03 +0000

    Add Benchmark

---
+++ Taylor/LargestSquareResult.cs
@@ -0,0 +1,10 @@
+﻿namespace Taylor
+{
+    public class LargestSquareResult
+    {
+        public int Size { get; set; }
+        public int StartX { get; set; }
+        public int StartY { get; set; }
+        // TODO - consider Point instead of separate x/y properties
+    }
+}
--- Taylor/Program.cs
+++ Taylor/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Text;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 namespace Taylor
 {
@@ -7,136 +7,26 @@ namespace Taylor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BenchmarkRunner.Run<ZeroArrayBenchmarks>();
+
         }
     }
-    public class BoolMatrix
+
+    [CoreJob]
+    public class ZeroArrayBenchmarks
     {
-        private int _size;
-        private bool[,] _array;
+        private BoolMatrix _matrix;
 
-        public BoolMatrix(int size)
+        [Params(10, 100, 1_000, 10_000)]
+        public int Size;
+
+        [GlobalSetup]
+        public void Setup()
         {
-            _size = size;
-            _array = new bool[size, size];
+            _matrix = new BoolMatrix(Size);
         }
 
-        public static BoolMatrix Parse(string text)
-        {
-            var rows = text.Split("\n");
-            var matrix = new BoolMatrix(rows.Length);
-            for (int i = 0; i < rows.Length; i++)
-            {
-                var values = rows[i].Trim().Split(" ");
-                if (values.Length != rows.Length)
-                {
-                    throw new ArgumentException("Rows and columns don't match");
-                }
-                for (int j = 0; j < values.Length; j++)
-                {
-                    bool value;
-                    switch (values[j])
-                    {
-                        case "0":
-                            value = false;
-                            break;
-                        case "1":
-                            value = true;
-                            break;
-                        default:
-                            throw new ArgumentException("Invalid character in input");
-                    }
-                    matrix[i, j] = value;
-                }
-            }
-            return matrix;
-        }
-        public override string ToString()
-        {
-            var buf = new StringBuilder(); // TODO estimate the size 
-            var rowSeparator = "";
-            for (int i = 0; i < _size; i++)
-            {
-                buf.Append(rowSeparator);
-                string columnSeparator = "";
-                for (int j = 0; j < _size; j++)
-                {
-                    buf.Append(columnSeparator);
-                    buf.Append(this[i, j] ? "1" : "0");
-                    columnSeparator = " ";
-                }
-                rowSeparator = "\r\n";
-            }
-            return buf.ToString();
-        }
-
-        public bool this[int x, int y]
-        {
-            get => _array[x, y];
-            set => _array[x, y] = value;
-        }
-
-        public LargestSquareResult GetLargestSquare()
-        {
-            var result = new LargestSquareResult
-            {
-                Size = 0,
-                StartX = -1,
-                StartY = -1,
-            };
-
-            for (int i = 0; i < _size; i++)
-            {
-                for (int j = 0; j < _size; j++)
-                {
-                    var size = GetSizeOfLargestSquare(i, j);
-                    if (size > result.Size)
-                    {
-                        result.Size = size;
-                        result.StartX = j;
-                        result.StartY = i;
-                    }
-                }
-            }
-            // TODO - short circuit if largest possible square for a given i/j is smaller than the current largest found
-
-            return result;
-        }
-        private int GetSizeOfLargestSquare(int startX, int startY)
-        {
-            int largestSquare = 0;
-            while (startX + largestSquare < _size && startY + largestSquare < _size)
-            {
-                // check new x-axis values
-                for (int i = startX; i <= startX + largestSquare; i++)
-                {
-                    if (!this[i, startY + largestSquare])
-                    {
-                        // found a false - return current largest square
-                        return largestSquare;
-                    }
-                }
-
-                // check new y-axis values (we can skip the last value as it overlaps with the x-axis checks)
-                for (int j = startY; j < startY + largestSquare; j++)
-                {
-                    if (!this[startX + largestSquare, j])
-                    {
-                        // found a false - return current largest square
-                        return largestSquare;
-                    }
-                }
-                largestSquare++;
-            }
-
-            return largestSquare;
-        }
-    }
-    public class LargestSquareResult
-    {
-        public int Size { get; set; }
-        public int StartX { get; set; }
-        public int StartY { get; set; }
-        // TODO - consider Point instead of separate x/y properties
+        [Benchmark]
+        public LargestSquareResult GetLargestSquare() => _matrix.GetLargestSquare();
     }
 }
